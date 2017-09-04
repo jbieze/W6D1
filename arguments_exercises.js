cfb7e89e3909a9a0be765d8e7360fea19937d7a3
Joey Bieze <joseph.bieze@gmail.com>
2017-09-04 12:00:53 -0700

    Start myBind

--- arguments_exercises.js
+++ arguments_exercises.js
@@ -15,3 +15,27 @@ const sumRest = function sumRest(...nums) {
 
   return total;
 };
+
+// // simple myBind with no args
+// Function.prototype.myBind = function (ctx) {
+//   return () => this.apply(ctx);
+// };
+//
+// // myBind with arguments
+// Function.prototype.myBind = function (ctx, ...bindArgs) {
+//   return (...callArgs) => {
+//     return this.apply(ctx, bindArgs.concat(callArgs));
+//   };
+// };
+
+const myBindArgs = function myBindArgs(args) {
+  const givenArgs = this;
+  return function () {
+    const Args = Array.from(arguments);
+    return givenArgs.apply(args, arguments.slice(1));
+  };
+};
+
+const myBindRest = function myBindRest(...args) {
+
+};
