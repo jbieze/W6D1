05023f62d0193411fcd41149020427e12386fb92
Joey Bieze <joseph.bieze@gmail.com>
2017-09-04 17:24:14 -0700

    Phase I: MovingObject.prototype.move and Util module

--- Asteroids/lib/moving_object.js
+++ Asteroids/lib/moving_object.js
@@ -12,4 +12,16 @@ const MovingObject = function MovingObject(options) {
   this.color = options.color;
 };
 
+MovingObject.prototype.draw = function (ctx) {
+  ctx.beginPath();
+  ctx.arc(this.pos[0], this.pos[1], this.rad, 0, 2 * Math.PI, true);
+  ctx.fillStyle = this.color;
+  ctx.fill();
+};
+
+MovingObject.prototype.move = function (timeDelta) {
+
+  this.pos = this.pos[0] + this.vel;
+};
+
 module.exports = MovingObject;
--- Asteroids/lib/utils.js
+++ Asteroids/lib/utils.js
@@ -1,5 +1,10 @@
 // Utility code, especially vector math stuff.
-const Util = function Util() {
+const Util =  {
+
+  inherits(childClass, baseClass) {
+    childClass.prototype = Object.create(baseClass.prototype);
+    childClass.prototype.constructor = childClass;
+  }
 
 };
 
